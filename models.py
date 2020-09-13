bf283e866259198968360eb8d5759bed42d02392
Rafal Jedel <noahh.r@gmail.com>
2020-09-13 19:37:17 +0200

    add class Message

--- models.py
+++ models.py
@@ -24,18 +24,28 @@ class User:
 
     def safe_to_db(self, cursor):
         if self._id == -1:
-            sql = """INSERT INTO users(username, hashed_password)
-                            VALUES(%s, %s) RETURNING id"""
+            sql = "INSERT INTO users (username, hashed_password) VALUES (%s, %s) RETURNING id"
             values = (self.username, self.hashed_password)
             cursor.execute(sql, values)
             self._id = cursor.fetchone()[0]  # or cursor.fetchone()['id']
             return True
         else:
-            sql = """UPDATE Users SET username=%s, hashed_password=%s
-                           WHERE id=%s"""
+            sql = "UPDATE users SET username=%s, hashed_password=%s WHERE id=%s"
             values = (self.username, self.hashed_password, self.id)
             cursor.execute(sql, values)
             return True
+
+    @staticmethod
+    def load_user_by_username(cursor, username):
+        sql = "SELECT id, username, hashed_password FROM users WHERE username=%s"
+        cursor.execute(sql, (username,))
+        data = cursor.fetchone()
+        if data:
+            id_, username, hashed_password = data
+            loaded_user = User(username)
+            loaded_user._id = id_
+            loaded_user._hashed_password = hashed_password
+            return loaded_user
 
     @staticmethod
     def load_user_by_id(cursor, id_):
@@ -68,3 +78,53 @@ class User:
         cursor.execute(sql, (self.id,))
         self._id = -1
         return True
+
+
+class Message:
+    def __init__(self, from_id, to_id, text):
+        self._id = -1
+        self.from_id = from_id
+        self.to_id = to_id
+        self.text = text
+        self._creation_date = None
+
+    @property
+    def creation_date(self):
+        return self._creation_date
+
+    @property
+    def id(self):
+        return self._id
+
+    @staticmethod
+    def load_all_messages(cursor, user_id=None):
+        if user_id:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages WHERE to_id=%s"
+            cursor.execute(sql, (user_id,))
+        else:
+            sql = "SELECT id, from_id, to_id, text, creation_date FROM messages"
+            cursor.execute(sql)
+        messages = []
+        for row in cursor.fetchall():
+            id_, from_id, to_id, text, creation_date = row
+            loaded_message = Message()
+            loaded_message._id = id_
+            loaded_message.from_id = from_id
+            loaded_message.to_id = to_id
+            loaded_message.text = text
+            loaded_message.creation_date = creation_date
+            messages.append(loaded_message)
+        return messages
+
+    def safe_to_db(self, cursor):
+        if self._id == -1:
+            sql = "INSERT INTO messages (from_id, to_id, text) VALUES (%s, %s, %s) RETURNING id, creation_date"
+            values = (self.from_id, self.to_id, self.text)
+            cursor.execute(sql, values)
+            self._id, self._creation_date = cursor.fetchone()
+            return True
+        else:
+            sql = "UPDATE messages SET from_id=%s, to_id=%s, text=%s WHERE id=%s"
+            values = (self.from_id, self.to_id, self.text, self.id)
+            cursor.execute(sql, values)
+            return True
